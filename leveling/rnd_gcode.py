a56514fffebcf370fee179c8d08db35a01e6c934
squirrel <squirrelcze@gmail.com>
2015-10-22 16:54:31 +0200

    remodified to make pattern, also using p3 O:)

--- leveling/rnd_gcode.py
+++ leveling/rnd_gcode.py
@@ -1,5 +1,24 @@
+#!/usr/bin/python3
+
 import os, sys, random
+from math import *
 
 if __name__=="__main__":
-  for x in range(990):
-    print 'G81 X{} Y{}'.format(round(random.random()*70, 5), round(random.random()*70, 5))
+
+  offset = [25, 25, 0]
+
+  points = list()
+
+  for i in range(0, int(pi*2*3), 1):
+      points.append([
+          i*sin(i),
+          i*cos(i),
+          -1
+      ])
+
+  for point in points:
+      point = [point[i] + offset[i] for i in range(len(point))]
+      # G01 is generate by pcb2gcode
+      print('G01 X{} Y{} Z{}'.format(*point))
+    
+
