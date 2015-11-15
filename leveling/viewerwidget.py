e6c10490289451764cfc1a9b3aaec8fda738bac7
Maros Zatko <mzatko@redhat.com>
2015-11-15 02:46:54 +0100

    viewewidget: MSAA, center to center of mass

--- leveling/viewerwidget.py
+++ leveling/viewerwidget.py
@@ -18,12 +18,18 @@ class ViewerWidget(QGLWidget):
     gcodelist = -1
 
     def __init__(self, parent = None):
-        super(ViewerWidget, self).__init__(parent)
+        fmt = QGLFormat.defaultFormat()
+        fmt.setSampleBuffers(True)
+        fmt.setSamples(32)
+
+        super(ViewerWidget, self).__init__(fmt, parent)
         self.gcode_points = []
         self.oldpos = 0, 0
         self.filename = ''
         self.objsize = 0, 0, 0
         self.gcodesize = 0
+        self._oc = (0, 0, 0)
+        self._ogc = None
 
     def object_size(self, obj):
       try:
@@ -43,13 +49,25 @@ class ViewerWidget(QGLWidget):
       except ValueError:
         return 0,0,0
 
+    def _recalc_object_center(self, obj):
+        if len(obj) > 2:
+          obj = obj[1:-1]
+        x = sum(map(lambda _x: float(_x['X']), obj))
+        y = sum(map(lambda _x: float(_x['Y']), obj))
+        z = sum(map(lambda _x: float(_x['Z']), obj))
+        l = len(obj)
+        if l == 0:
+          self._oc = 0, 0, 0
+        self._oc = (x/l, y/l, z/l)
+
     def resetView(self):
       ViewerWidget.rotx = 0
       ViewerWidget.roty = 0
-      s = self.object_size(self.gcode_points)
+      self._recalc_object_center(self.gcode_points)
       ViewerWidget.tranx = 0
       ViewerWidget.trany = 0
-      ViewerWidget.tranz = -s[1]*3
+      sz = self.object_size(self.gcode_points)
+      ViewerWidget.tranz = (self._oc[1]+sz[1])#*2.2
 
       self.update()
 
@@ -62,17 +80,22 @@ class ViewerWidget(QGLWidget):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
         L = 0
+        glRotate(180, 0, 0, 1)
+        glRotate(180, 0, 1, 0)
         glDisable(GL_DEPTH_TEST)
         glColor3f(0.8, 0.8, 0.8)
         self.renderText(20, 20, '{} | {} cmds'.format(
           self.filename, self.gcodesize))
         self.renderText(20, 40, '{:.4f} x {:.4f} x {:.4f} mm'.format(
           self.objsize[0], self.objsize[1], self.objsize[2]))
-#        self.renderText(20, 40, str(self.gcodesize) + ' cmds')
-#        self.renderText(20, 60, str(self.objsize) + ' mm')
+        self.renderText(20, 60, '{:.4f} , {:.4f} deg'.format(
+          self.rotx, self.roty))
+        self.renderText(20, 80, 'v: {:.4f} , {:.4f} , {:.4f}'.format(
+          ViewerWidget.tranx, ViewerWidget.trany, ViewerWidget.tranz))
+        self.renderText(20, 100, '{:.4f} x {:.4f} x {:.4f} mm'.format(
+          self._oc[0], self._oc[1], self._oc[2]))
         glEnable(GL_DEPTH_TEST)
 
-        glRotatef(90, 1, 0, 0)
         glEnable(GL_MULTISAMPLE)
 
         glPushMatrix()
@@ -80,37 +103,24 @@ class ViewerWidget(QGLWidget):
           ViewerWidget.gcodelist = glGenLists(1)
           glShadeModel(GL_SMOOTH)
           glNewList(ViewerWidget.gcodelist, GL_COMPILE_AND_EXECUTE)
-          glBegin(GL_LINES)
-          for i in range(len(self.gcode_points) - 1):
-            start = self.gcode_points[i]
-            end = self.gcode_points[i+1]
-            try:
-              glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.2, 0.2, 0.2, 1.0])
-              glColor3f(i/float(len(self.gcode_points)), 0.5, float(start['L']))
-              glNormal3f(0,0,1)
-              glVertex3f(*map(float, [start['X'], start['Y'], start['Z']]))
-
-              glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [0.2, 0.2, 0.2, 1.0])
-              glColor3f((i+1)/float(len(self.gcode_points)), 0.5, float(end['L']))
-              glNormal3f(0,0,1)
-              glVertex3f(*map(float, [end['X'], end['Y'], end['Z']]))
-            except ValueError as e:
-              print>>sys.stderr, e.message
-              glVertex3f(0,0,0)
+          glBegin(GL_LINE_STRIP)
+          for i, p in enumerate(self.gcode_points):
+            _x, _y, _z = map(float, [p['X'], p['Y'], p['Z']])
+            glColor3f(i/float(len(self.gcode_points)), 0.5, float(p['L']))
+            glVertex3f(_x, _y, _z)
           glEnd()
           try:
             glEndList()
           except:
-            print 'wtf'
             wtf = True
 
           glCallList(self.gcodelist)
         else:
-          glTranslate(ViewerWidget.tranx, ViewerWidget.tranz, ViewerWidget.trany)
+          glTranslate(ViewerWidget.tranx, ViewerWidget.trany, ViewerWidget.tranz)
           glScale(ViewerWidget.scale, ViewerWidget.scale, ViewerWidget.scale)
           glRotate(float(ViewerWidget.roty), 1.0, 0.0, 0.0)
           glRotate(float(ViewerWidget.rotx), 0.0, 0.0, 1.0)
-          glTranslate(-self.objsize[0]/2, -self.objsize[1]/2, -self.objsize[2]/2)
+          glTranslate(-self._oc[0], -self._oc[1], -self._oc[2])
           glCallList(ViewerWidget.gcodelist)
         glPopMatrix()
         self.swapBuffers()
@@ -153,6 +163,7 @@ class ViewerWidget(QGLWidget):
       self.filename = filename
       self.gcodesize = len(self.gcode_points)
       self.objsize = s
+      self._recalc_object_center(self.gcode_points)
       self.updateGL()
 
     def mousePressEvent(self, m):
@@ -160,17 +171,23 @@ class ViewerWidget(QGLWidget):
 
     def wheelEvent(self, m):
 #      ViewerWidget.scale *= (120+m.angleDelta().y()/12)/120.0
-      ViewerWidget.tranz += m.angleDelta().y()/24.0
+      ViewerWidget.tranz -= m.angleDelta().y()/24.0
       self.update()
 
     def mouseMoveEvent(self, m):
       button = int(m.buttons())
       if button == 1:
-        ViewerWidget.rotx += (self.oldpos[0]-m.x())/10.0
         ViewerWidget.roty -= (self.oldpos[1]-m.y())/10.0
+        if (int(ViewerWidget.roty) % 360) > 0 and (int(ViewerWidget.roty) % 360) < 180:
+          ViewerWidget.rotx -= (self.oldpos[0]-m.x())/10.0
+        else:
+          ViewerWidget.rotx += (self.oldpos[0]-m.x())/10.0
       elif button == 2:
-        ViewerWidget.tranx -= (self.oldpos[0]-m.x())/4.0
-        ViewerWidget.trany -= (self.oldpos[1]-m.y())/4.0
+        c = 1
+        if ViewerWidget.tranz < 0:
+          c = -ViewerWidget.tranz/100.0
+        ViewerWidget.tranx -= c*(self.oldpos[0]-m.x())/4.0
+        ViewerWidget.trany -= c*(self.oldpos[1]-m.y())/4.0
       self.oldpos = m.x(), m.y()
       self.update()
 
